e93ae3e4cbb7e76656ad53c9222bfa8ea95081f3
grj1234 <grj1234@protonmail.com>
2023-04-04 18:51:31 +0900

    Release v0.3

--- DecMebiusWAV.php
+++ DecMebiusWAV.php
@@ -1,13 +1,13 @@
 <?php
 // -----------------------------------------
-// DecMebiusWAV Version 0.2 by grj1234
+// DecMebiusWAV Version 0.3 by grj1234
 // https://github.com/grj1234/DecMebiusWAV
 // -----------------------------------------
 setlocale(LC_ALL,'');
 ini_set('error_log','php://stderr');
-$version="0.2";
+$version="0.3";
 $debugmode=false;
-$decryption_buffer_size=1048576;
+$read_buffer_size=1048576;
 $message_handle=fopen("php://stderr","wb");
 fwrite($message_handle,
 	"DecMebiusWAV Version ".$version." by grj1234".PHP_EOL.
@@ -19,18 +19,18 @@ if($argc<3) {
 	fwrite($message_handle,
 		"Usage: DecMebiusWAV <Input file> <Output file> [XOR key]".PHP_EOL.
 		PHP_EOL.
-		"This tool is to decrypt XOR-encrypted WAV files which used in some games developed by Studio Mebius and related developers (e.g. Studio Ring).".PHP_EOL.
+		"This tool decrypts Mebius engine's XOR-encrypted WAV files (also called Mebius KOE files by GARbro) which used in some games developed by Studio Mebius and related developers (e.g. Studio Ring).".PHP_EOL.
 		"If you want to output to stdout, specify - as output file. (Input from stdin is not supported)".PHP_EOL.
 		"You can't use wildcard for input file and output file.".PHP_EOL.
-		"DecMebiusWAV will overwrite the output file without confirmation even if it exists.".PHP_EOL.
-		'You can specify the hex bytes of binary as XOR key. (Example: "B06FA4D7")'.PHP_EOL.
-		"When you don't specify the XOR key, DecMebiusWAV will load external binary file as XOR key.".PHP_EOL.
-		'The file name of key file must be "(name).(ext)key" (for a single file), or ".(ext)key" (for the whole folder).'.PHP_EOL.
-		"If you don't specify the key and the external key not found, DecMebiusWAV will use default XOR key. ".'(Default key: "AA")'.PHP_EOL
+		"This tool needs the XOR key to decrypt, so you need to specify the key by argument or external file.".PHP_EOL.
+		"To specify XOR key by argument, you need to use the hex-encoded binary string. ".'(Example: "B06FA4D7")'.PHP_EOL.
+		"When you don't specify the XOR key by argument, DecMebiusWAV will load external binary file as XOR key.".PHP_EOL.
+		"The file name of key file must be ".'"(name).(ext)key"'." (for a single file), or ".'".(ext)key"'." (for the whole folder).".PHP_EOL
 	);
 	fclose($message_handle);
 	exit;
 }
+
 $in_file=$argv[1];
 $out_type=($argv[2]=="-");
 $out_file=$out_type?'php://stdout':$argv[2];
@@ -50,7 +50,7 @@ if(!file_exists($in_file)) {
 	exit(1);
 }
 $in_pathinfo=pathinfo($in_file);
-$key_ext=isset($in_pathinfo{"extension"})?(".".$in_pathinfo{"extension"}."key"):".key";
+$key_ext=isset($in_pathinfo["extension"])?(".".$in_pathinfo["extension"]."key"):".key";
 if(isset($argv[3])) {
 	// XOR key specified as argument
 	$xor_key=hex2bin($argv[3]);
@@ -64,9 +64,9 @@ if(isset($argv[3])) {
 		exit(1);
 	}
 	fwrite($message_handle,"Using external XOR key file for a single file".PHP_EOL);
-} else if(file_exists($in_pathinfo{"dirname"}."/".$key_ext)) {
+} else if(file_exists($in_pathinfo["dirname"]."/".$key_ext)) {
 	// load key file (for the whole folder)
-	$xor_key=file_get_contents($in_pathinfo{"dirname"}."/".$key_ext);
+	$xor_key=file_get_contents($in_pathinfo["dirname"]."/".$key_ext);
 	if($xor_key===false){
 		fwrite($message_handle,"Error: External XOR key file for the whole folder found, but loading failed".PHP_EOL);
 		fclose($message_handle);
@@ -74,9 +74,10 @@ if(isset($argv[3])) {
 	}
 	fwrite($message_handle,"Using external XOR key file for the whole folder".PHP_EOL);
 } else {
-	// key file not found (0xAA)
-	$xor_key=hex2bin('AA');
-	fwrite($message_handle,'XOR key not specified and external key file not found, using default XOR key'.PHP_EOL);
+	// key file not found
+	fwrite($message_handle,"Error: XOR key not specified and external key file not found".PHP_EOL);
+	fclose($message_handle);
+	exit(1);
 }
 fwrite($message_handle,'XOR key: '.strtoupper(bin2hex($xor_key)).PHP_EOL);
 fwrite($message_handle,'Input file: '.$in_file.PHP_EOL);
@@ -102,7 +103,7 @@ if(($in_riff_header_parsed['file_type']!="RIFF")||($in_riff_header_parsed['riff_
 	exit(1);
 }
 if(filesize($in_file)!=($in_riff_header_parsed['size']+8)) {
-	fwrite($message_handle,"Warning: The information of file size included in the RIFF header did not match the actual file size. The input file might be corrupted.");
+	fwrite($message_handle,"Warning: The information of file size included in the RIFF header did not match the actual file size. The input file might be truncated or corrupted.");
 }
 $out_handle=@fopen($out_file,"wb");
 if(!is_resource($out_handle)) {
@@ -112,26 +113,36 @@ if(!is_resource($out_handle)) {
 	exit(1);
 }
 fwrite($out_handle,$in_riff_header);
-for(;;) {
+while(true) {
 	$chunk_info=fread($in_handle,8);
 	$chunk_info_parsed=unpack("a4chunk_type/Vchunk_size",$chunk_info);
-	if($chunk_info_parsed['chunk_type']=='data') {
-		fwrite($out_handle,$chunk_info);
-		break;
-	} else {
-		fwrite($out_handle,$chunk_info.fread($in_handle,$chunk_info_parsed['chunk_size']));
+	fwrite($out_handle,$chunk_info);
+	if($chunk_info_parsed['chunk_type']!='data') {
+		$remaining_chunk_size=$chunk_info_parsed['chunk_size'];
+		while(true) {
+			if($remaining_chunk_size>$read_buffer_size) {
+				fwrite($out_handle,fread($in_handle,$read_buffer_size));
+				$remaining_chunk_size-=$read_buffer_size;
+			} else {
+				fwrite($out_handle,fread($in_handle,$remaining_chunk_size));
+				break;
+			}
+		}
 	}
+	unset($chunk_info);
+	unset($chunk_info_parsed);
+	unset($remaining_chunk_size);
 }
 $encrypted_offset=ftell($in_handle);
-for(;ftell($in_handle)!=filesize($in_file);){
+while(ftell($in_handle)!=filesize($in_file)){
 	$encrypted_data_offset=ftell($in_handle)-$encrypted_offset;
-	$encrypted_data=fread($in_handle,$decryption_buffer_size);
+	$encrypted_data=fread($in_handle,$read_buffer_size);
 	$encrypted_data_size=strlen($encrypted_data);
 	$xor_key_for_buffer=substr(str_repeat($xor_key,(ceil($encrypted_data_size/strlen($xor_key))+1)),($encrypted_data_offset%strlen($xor_key)),$encrypted_data_size);
 	$decrypted_data=($encrypted_data^$xor_key_for_buffer);
 	unset($encrypted_data);
 	unset($xor_key_for_buffer);
-	fwrite($message_handle,$debugmode?("[DEBUG] input_file_ptr:".ftell($in_handle).", buffer_size:".strlen($decrypted_data).", xor_key_offset:".strval($encrypted_data_offset%strlen($xor_key)).PHP_EOL):"");
+	fwrite($message_handle,$debugmode?("[DEBUG] input_pos:".ftell($in_handle).", data_size:".strlen($decrypted_data).", key_offset:".strval($encrypted_data_offset%strlen($xor_key)).PHP_EOL):"");
 	fwrite($out_handle,$decrypted_data);
 	unset($decrypted_data);
 }
--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-This tool has many bugs and vulnerabilities. I recommend to use [vgmstream](https://vgmstream.org/) instead.
+__This tool has many bugs and vulnerabilities. I recommend to use [vgmstream](https://vgmstream.org/) instead.__  
 
-# DecMebiusWAV Version 0.2
+# DecMebiusWAV Version 0.3
 
-This tool decrypts Mebius engine's XOR-encrypted WAV files (also called Mebius KOE files by GARbro) which used in some games developed by Studio Mebius and related developers (e.g. Studio Ring).  
+This tool decrypts Mebius engine's XOR-encrypted WAV files (also called Mebius KOE files by GARbro), which used in some games developed by Studio Mebius and related developers (e.g. Studio Ring).  
 
 ## Usage
 Usage: `DecMebiusWAV <Input file> <Output file> [XOR key]`  
   
 If you want to output to stdout, specify - as output file. (Input from stdin is not supported)  
 You can't use wildcard for input file and output file.  
-You can specify the hex bytes of binary as XOR key. (Example: "B06FA4D7")  
-When you don't specify the XOR key, DecMebiusWAV will load external binary file as XOR key.  
+This tool needs the XOR key to decrypt, so you need to specify the key by argument or external file.  
+To specify XOR key by argument, you need to use the hex-encoded binary string. (Example: "B06FA4D7")  
+When you don't specify the XOR key by argument, DecMebiusWAV will load external binary file as XOR key.  
 The file name of key file must be "(name).(ext)key" (for a single file), or ".(ext)key" (for the whole folder).  
-If you don't specify the key and the external key not found, DecMebiusWAV will use default XOR key. (Default key: "AA")  
 
 ## Notes
 - You have to install PHP and modify the PATH environment variable to include the PHP directory to run this tool.
-- I checked this tool can be run on PHP 7.0.7, but I didn't check whether can run on the other versions of PHP or not.
+- I checked this tool can be run on PHP 7.0, but I didn't check whether can run on the other versions of PHP or not.
 	- The newer version of PHP also can run this tool, probably.
 - DecMebiusWAV will overwrite the output file without confirmation even if it exists.
 
@@ -25,8 +25,14 @@ All source code in this repository is released under the [NYSL Version 0.9982](h
 File "LICENSE_ja" contains the license text of NYSL, written in Japanese. File "LICENSE" contains the unofficial English translation of NYSL.
 
 ## Changelog
+### Version 0.3 (2023-04-04)
+- Fixed that DecMebiusWAV.php script fails to run on PHP 8.0+ due to curly brackets for accessing array elements
+- Fixed that DecMebiusWAV.php script may cause out of memory error when searching RIFF chunks
+- Deleted the default key
+- Improved the translation of license text
+
 ### Version 0.2 (2021-01-19)
-- Fixed DecMebiusWAV.php script fails to open the key file for the whole folder
+- Fixed that DecMebiusWAV.php script fails to open the key file for the whole folder
 - Added license text
 
 ### Version 0.1 (2020-04-28)
